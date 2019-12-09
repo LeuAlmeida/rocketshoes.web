a4a7b81aabf750b0fb218ac1bda633b087e1ecd8
LeuAlmeida <leo@webid.net.br>
2019-12-09 20:26:27 -0300

    Incrementing product amount using immer

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -37,7 +37,7 @@ function Cart({ cart }) {
                   <button type="button">
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                  <input type="number" readOnly value={2} />
+                  <input type="number" readOnly value={product.amount} />
                   <button type="button">
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
