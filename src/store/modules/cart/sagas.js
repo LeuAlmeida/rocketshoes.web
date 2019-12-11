ba67bcc8cabfdde1c7575d0940b084c30ee18a56
LeuAlmeida <leo@webid.net.br>
2019-12-11 11:17:49 -0300

    Improving add to cart using saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,20 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
 import { addToCartSuccess } from './actions';
 
 function* addToCart({ id }) {
   const response = yield call(api.get, `/products/${id}`);
 
-  yield put(addToCartSuccess(response.data));
+  const data = {
+    ...response.data,
+    amount: 1,
+    priceFormatted: formatPrice(response.data.price),
+  };
+
+  yield put(addToCartSuccess(data));
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
